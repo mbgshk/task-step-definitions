64cf31bc2685fdd0d13387f11bc4b2d599632e22
mbgshk <mbgshk@gmail.com>
2022-07-16 09:50:47 +0700

    Page Object Pattern

--- searchPage.js
+++ searchPage.js
@@ -1,2 +1,18 @@
 const URL = 'http://zero.webappsecurity.com/index.html'
 const SEARCH = '#searchTerm'
+
+class SearchPage {
+    static visit() {
+        cy.visit(URL)
+    }
+
+    static search() {
+        cy.get(SEARCH).should('be.visible')
+    }
+
+    static fillSearch(search) {
+        cy.get(SEARCH).type(search)
+    }
+}
+
+export default SearchPage
